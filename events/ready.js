65cc428a0e535d80b91396ffbae7cf658a612cbd
Glitch (natsume-test) <none>
2020-12-08 17:22:12 +0000

    🐉📌 Checkpoint ./events/ready.js:6147130/83

--- events/ready.js
+++ events/ready.js
@@ -6,6 +6,6 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-client.user.setPresence({ activity: { name: `${ayarlar.prefix}yardım /\ ${client.guilds.cache.size} sunucu /\ `}, status: 'dnd'})
+client.user.setPresence({ activity: { name: `${ayarlar.prefix}yardım`}, status: 'idle'})
 console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.cache.size + ` adet kanala, ` + client.guilds.cache.size + ` adet sunucuya ve ` + client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
