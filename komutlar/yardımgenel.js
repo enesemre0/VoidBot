6508c71be113bd61c26384260eba9ce9df44bd34
Glitch (natsume-test) <none>
2020-12-14 11:03:20 +0000

    👻🍽 Checkpoint ./komutlar/yardımgenel.js:6147130/9038

--- komutlar/yardımgenel.js
+++ komutlar/yardımgenel.js
@@ -1,26 +1,26 @@
 const Discord = require('discord.js')
 const ayarlar = require('../ayarlar.json');
 
-let prefix = ayarlar.prefix
+let prefix = ayarlar.prefix;
 
 exports.run = async(client, message, args) => {
 
-let desteksunucusu = "https://discord.gg/UZFxuEfxtQ"
+let desteksunucusu = "https://discord.gg/UZFxuEfxtQ";
 
 if(!args[0]) {
     const embed = new Discord.MessageEmbed()
-    .setAuthor('Yardım', client.user.avatarURL())
+    .setAuthor('Genel Komutlar', client.user.avatarURL())
     .setDescription('[VoidBot\'u Hemen Ekle](https://discord.com/oauth2/authorize?client_id=723523223047766046&scope=bot&permissions=8)')
-    .addField('Genel Komutlar:', 
-    ``)
+    .addField('Komutlar:', 
+    `${prefix}rolbilgi`)
     .setThumbnail(client.user.avatarURL())
     .setColor("RANDOM")
-    .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.avatarURL())
-    message.channel.send(embed)
+    .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.avatarURL());
+    message.channel.send(embed);
 };}
 exports.conf = {
-    aliases: []
+    aliases: ["genel"]
 }
 exports.help = {
-    name: "yardım"
+    name: "yardım genel"
 }
