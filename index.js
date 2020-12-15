72cb1247979530d71a64ffa474387eeab645154f
Glitch (natsume-test) <none>
2020-12-15 09:22:23 +0000

    🚠🐂 Checkpoint ./komutlar/afk.js:6147130/3028 ./komutlar/istatistik.js:6147130/1345 ./index.js:6147130/9110

--- index.js
+++ index.js
@@ -1,167 +1,136 @@
-const http = require('http');
-const express = require('express');
-const moment = require('moment')
-moment.locale("tr")
+const http = require("http");
+const express = require("express");
+const moment = require("moment");
+moment.locale("tr");
 const app = express();
 app.get("/", (request, response) => {
-console.log(moment().format("LLLL") + " BERK //\\ CE");
-response.sendStatus(200);
+  console.log(moment().format("LLLL") + " BERK //\\ CE");
+  response.sendStatus(200);
 });
 app.listen(process.env.PORT);
 setInterval(() => {
-http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000)
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const ayarlar = require('./ayarlar.json');
-const chalk = require('chalk');
-const Canvas = require('canvas')
-const snekfetch = require('snekfetch');
-const fs = require('fs');
-const db = require('quick.db')
-const ms = require('parse-ms');
-require('moment-duration-format')
-require('./util/eventLoader')(client);
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const Canvas = require("canvas");
+const snekfetch = require("snekfetch");
+const fs = require("fs");
+const db = require("quick.db");
+const ms = require("parse-ms");
+require("moment-duration-format");
+require("./util/eventLoader")(client);
 
 var prefix = ayarlar.prefix;
 
 const log = message => {
-console.log(`${moment().format("DD MMMM YYYY HH:mm")} ${message}`);
+  console.log(`${moment().format("DD MMMM YYYY HH:mm")} ${message}`);
 };
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-fs.readdir('./komutlar/', (err, files) => {
-if (err) console.error(err);
-log(`${files.length} adet komut yükleniyor;`);
-files.forEach(f => {
-let props = require(`./komutlar/${f}`);
-log(`Komut: ${props.help.name}`);
-client.commands.set(props.help.name, props);
-props.conf.aliases.forEach(alias => {
-client.aliases.set(alias, props.help.name);
-});
-});
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} adet komut yükleniyor;`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Komut: ${props.help.name}`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
 });
 
 client.reload = command => {
-return new Promise((resolve, reject) => {
-try {
-delete require.cache[require.resolve(`./komutlar/${command}`)];
-let cmd = require(`./komutlar/${command}`);
-client.commands.delete(command);
-client.aliases.forEach((cmd, alias) => {
-if (cmd === command) client.aliases.delete(alias);
-});
-client.commands.set(command, cmd);
-cmd.conf.aliases.forEach(alias => {
-client.aliases.set(alias, cmd.help.name);
-});
-resolve();
-} catch (e){
-reject(e);
-}
-});
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 client.load = command => {
-return new Promise((resolve, reject) => {
-try {
-let cmd = require(`./komutlar/${command}`);
-client.commands.set(command, cmd);
-cmd.conf.aliases.forEach(alias => {
-client.aliases.set(alias, cmd.help.name);
-});
-resolve();
-} catch (e){
-reject(e);
-}
-});
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 client.unload = command => {
-return new Promise((resolve, reject) => {
-try {
-delete require.cache[require.resolve(`./komutlar/${command}`)];
-let cmd = require(`./komutlar/${command}`);
-client.commands.delete(command);
-client.aliases.forEach((cmd, alias) => {
-if (cmd === command) client.aliases.delete(alias);
-});
-resolve();
-} catch (e){
-reject(e);
-}
-});
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 client.elevation = message => {
-if(!message.guild) return;
+  if (!message.guild) return;
 
-let permlvl = 0;
-if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
-if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-if (message.author.id === ayarlar.sahip) permlvl = 4;
-return permlvl;
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
 };
 
 // KODLAR BURADAN SONRA \\
 
-
-
 client.login(process.env.TOKEN);
 
-client.on("userUpdate", async (oldUser, newUser) => {
-  if (oldUser.username !== newUser.username) {
-  const tag = 's'
-  const sunucu = '705673781561131048'
-  const kanal = '705802740177043457'
-  const rol = '705695900885844031'
+client.on("message", async msg => {
+  if (!msg.guild) return;
+  if (msg.content.startsWith(ayarlar.prefix + "afk")) return;
 
-  try {
+  let afk = msg.mentions.users.first();
 
-  if (newUser.username.includes(tag) && !client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.cache.has(rol)) {
-  await client.channels.cache.get(kanal).send(new Discord.MessageEmbed().setColor("GREEN").setDescription(`${newUser} ${tag} Tagımızı Aldığı İçin <@&${rol}> Rolünü Verdim`));
-  await client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.add(rol);
-  await client.guilds.cache.get(sunucu).members.cache.get(newUser.id).send(`Selam ${newUser.username}, Sunucumuzda ${tag} Tagımızı Aldığın İçin ${client.guilds.cache.get(sunucu).roles.cache.get(rol).name} Rolünü Sana Verdim!`)
+  const kisi = db.fetch(`afkid_${msg.author.id}_${msg.guild.id}`);
+
+  const isim = db.fetch(`afkAd_${msg.author.id}_${msg.guild.id}`);
+  if (afk) {
+    const sebep = db.fetch(`afkSebep_${afk.id}_${msg.guild.id}`);
+    const kisi3 = db.fetch(`afkid_${afk.id}_${msg.guild.id}`);
+    if (msg.content.includes(kisi3)) {
+      msg.reply(`Etiketlediğiniz Kişi Afk \nSebep : ${sebep}`);
+    }
   }
-  if (!newUser.username.includes(tag) && client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.cache.has(rol)) {
-  await client.channels.cache.get(kanal).send(new Discord.MessageEmbed().setColor("RED").setDescription(`${newUser} ${tag} Tagımızı Çıkardığı İçin <@&${rol}> Rolünü Aldım`));
-  await client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.remove(rol);
-  await client.guilds.cache.get(sunucu).members.cache.get(newUser.id).send(`Selam **${newUser.username}**, Sunucumuzda ${tag} Tagımızı Çıkardığın İçin ${client.guilds.cache.get(sunucu).roles.cache.get(rol).name} Rolünü Senden Aldım!`)
+  if (msg.author.id === kisi) {
+    msg.reply(`Afk'lıktan Çıktınız`);
+    db.delete(`afkSebep_${msg.author.id}_${msg.guild.id}`);
+    db.delete(`afkid_${msg.author.id}_${msg.guild.id}`);
+    db.delete(`afkAd_${msg.author.id}_${msg.guild.id}`);
+    msg.member.setNickname(isim);
   }
-} catch (e) {
-console.log(`Bir hata oluştu! ${e}`)
- }
-}
-});
-client.on("message" , async msg => {
-  
-  if(!msg.guild) return;
-  if(msg.content.startsWith(ayarlar.prefix+"afk")) return; 
-  
-  let afk = msg.mentions.users.first()
-  
-  const kisi = db.fetch(`afkid_${msg.author.id}_${msg.guild.id}`)
-  
-  const isim = db.fetch(`afkAd_${msg.author.id}_${msg.guild.id}`)
- if(afk){
-   const sebep = db.fetch(`afkSebep_${afk.id}_${msg.guild.id}`)
-   const kisi3 = db.fetch(`afkid_${afk.id}_${msg.guild.id}`)
-   if(msg.content.includes(kisi3)){
-
-       msg.reply(`Etiketlediğiniz Kişi Afk \nSebep : ${sebep}`)
-   }
- }
-  if(msg.author.id === kisi){
-
-       msg.reply(`Afk'lıktan Çıktınız`)
-   db.delete(`afkSebep_${msg.author.id}_${msg.guild.id}`)
-   db.delete(`afkid_${msg.author.id}_${msg.guild.id}`)
-   db.delete(`afkAd_${msg.author.id}_${msg.guild.id}`)
-    msg.member.setNickname(isim)
-    
-  }
-  
 });
